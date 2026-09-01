e401c34d34b54bd4a300e6d9d859f78ecd1605b8
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-01 09:12:24 +0800

    Declare standalone native test source

--- tests/run_fd_dup_test.sh
+++ tests/run_fd_dup_test.sh
@@ -6,10 +6,11 @@ tmp_dir=$(mktemp -d)
 trap 'rm -rf "$tmp_dir"' EXIT
 
 cc=${CC:-gcc}
+NATIVE_TEST_SOURCE=tests/test_fd_dup.c
 
 "$cc" -m32 -std=gnu99 -O1 -g -Wall -Wextra -fno-builtin \
     -DHOSTED_TEST -ffunction-sections -fdata-sections \
     -Wl,--gc-sections \
-    "$repo_dir/tests/test_fd_dup.c" -o "$tmp_dir/test_fd_dup"
+    "$repo_dir/$NATIVE_TEST_SOURCE" -o "$tmp_dir/test_fd_dup"
 
 "$tmp_dir/test_fd_dup"
